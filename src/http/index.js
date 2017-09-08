3373f70d9db7fa7a22fe85f9d62f69d7a0fb2dce
Hugo de Moraes <hugoc.bmoraes@gmail.com>
2017-09-08 18:33:35 -0300

    fix API_URL

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,13 @@
 import axios from 'axios'
 
+const getAPIUrl = (API_URL) => {
+  if (API_URL !== undefined) {
+    return API_URL
+  }
+  return 'http://localhost:5000/api/'
+}
+
 export const HTTP = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: getAPIUrl(process.env.API_URL),
   headers: { 'Content-Type': 'application/json' }
 })
