0502de3659ed839652fef14254c397560031b758
Hugo de Moraes <hugoc.bmoraes@gmail.com>
2017-09-08 19:22:10 -0300

    HTTPS

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: 'http://icescreamapi.azurewebsites.net/api/',
+  baseURL: 'https://icescreamapi.azurewebsites.net/api/',
   headers: { 'Content-Type': 'application/json' }
 })
