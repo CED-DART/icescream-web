188356fa960e0b9ff30d96c1206ccc40ec64e558
Hugo de Moraes <hugoc.bmoraes@gmail.com>
2017-09-20 19:42:04 -0300

    fix server URL

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var serveStatic = require('serve-static')
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
