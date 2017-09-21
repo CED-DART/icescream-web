d8558f1ee3a503c553222e04bf5d25ee2a44875c
Hugo de Moraes <hugoc.bmoraes@gmail.com>
2017-09-21 02:12:03 -0300

    change deploy on heroku with history mode router

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const http = require("http")
+const fs = require("fs")
+const httpPort = 80
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+http.createServer((req, res) => {
+  fs.readFile("index.htm", "utf-8", (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.htm" file.')
+    }
 
-// handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')())
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8"
+    })
 
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+    res.end(content)
+  })
+}).listen(httpPort, () => {
+  console.log("Server listening on: http://localhost:%s", httpPort)
+})
